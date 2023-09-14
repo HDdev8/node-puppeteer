eb9fac2662f4fd7f947fc15598edc4d7e2c57d25
HDdev8 <114863680+HDdev8@users.noreply.github.com>
2023-09-13 22:49:08 -0400

    preliminary files to set up repo

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const path = require("node:path");
+require("dotenv").config({path: path.resolve(__dirname, "./.env")});
+
+const R_USERNAME = process.env.R_USERNAME;
+const R_PASSWORD = process.env.R_PASSWORD;
+const PORT = process.env.PORT || 3001;
+
+module.exports = {
+  R_USERNAME,
+  R_PASSWORD,
+  PORT,
+};
+
+module.exports = {R_USERNAME, R_PASSWORD, PORT};
